e15c100c7de56158b7d0bc70b89a8c0f8c215206
rocko2314243 <86879726+rocko2314243@users.noreply.github.com>
2022-04-25 15:48:19 +0300

    Update chat.js

--- mopeuniversal/modules/chat.js
+++ mopeuniversal/modules/chat.js
@@ -90,7 +90,7 @@ function devcommands(ws, msgData, writer, randomparseInt, entities, ws_new) {
                 break
             case "size":
                 let m = parseInt(initial[1])
-                if (m > -9999999) m = -9999999
+                if (m > 50) m = 500
                 ws.player.moreradius = m
                 break
             case "changeall":
